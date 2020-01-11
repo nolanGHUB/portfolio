2635c53837168727e71b63f1cd02ff993819a8da
nolanG <nolanrgiles@gmail.com>
2020-01-11 15:54:52 -0500

    added description

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -2,15 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <div>
-        Photo!
+    <div id="about" className="about center-alignment">
+      <div className="about-photo">
       </div>
-      <div>
-        Name!
-      </div>
-      <div>
-        Short Description!
+      <div className="about-description">
+        My name's Nolan and I have a passion for movies, music, fantasy/sci-fi fiction and full-stack programming. Love to learn, problem-solve with an attention to detail and create a better internet experience one page at a time.
       </div>
     </div>
   )
