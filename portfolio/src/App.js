fc1b7206a7876ee6253f2f7c6a10f174fa003c6d
nolanG <nolanrgiles@gmail.com>
2020-01-11 15:53:41 -0500

    Simple links to main components added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,16 +8,30 @@ import Portfolio from './components/Portfolio'
 import Technologies from './components/Technologies'
 import About from './components/About'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About />
-      <Portfolio />
-      <Technologies />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Header />
+        <About />
+        <Portfolio
+          portfolioList={this.state.portfolioList}
+        />
+        <Technologies
+          techList={this.state.techList}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
