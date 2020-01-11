91836830773f40443e354f8674b9dab3f90092e8
nolanG <nolanrgiles@gmail.com>
2020-01-11 16:39:58 -0500

    edited description

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-photo">
       </div>
       <div className="about-description">
-        My name's Nolan and I have a passion for movies, music, fantasy/sci-fi fiction and full-stack programming. Love to learn, problem-solve with an attention to detail and create a better internet experience one page at a time.
+        My name's Nolan and I have a passion for movies, music, fantasy/sci-fi fiction and full-stack programming. Love to learn, problem-solve with an attention to detail and creating a better internet experience one page at a time.
       </div>
     </div>
   )
