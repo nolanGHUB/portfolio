d34f7f9c549a8ea17d3b387e592f1ba62e919b1b
nolanG <nolanrgiles@gmail.com>
2020-01-11 16:00:03 -0500

    Added list of tech into state and map through in render to display a list with icons

--- portfolio/src/components/Technologies.js
+++ portfolio/src/components/Technologies.js
@@ -1,11 +1,97 @@
 import React from 'react'
 
-const Technologies = () => {
-  return (
-    <div>
-      <p>Hey I'm a Technologies!</p>
-    </div>
-  )
+class Technologies extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      techList: [
+        {
+          name: 'HTML5',
+          img: '/images/html.svg'
+        },
+        {
+          name: 'CSS3',
+          img: '/images/css.svg'
+        },
+        {
+          name: 'Javascript',
+          img: '/images/javascript.svg'
+        },
+        {
+          name: 'React',
+          img: '/images/react.svg'
+        },
+        {
+          name: 'jQuery',
+          img: '/images/jquery.svg'
+        },
+        {
+          name: 'Node.js',
+          img: '/images/node.svg'
+        },
+        {
+          name: 'Git',
+          img: '/images/git.svg'
+        },
+        {
+          name: 'Github',
+          img: '/images/github.svg'
+        },
+        {
+          name: 'Express',
+          img: '/images/express.svg'
+        },
+        {
+          name: 'Mysql',
+          img: '/images/mysql.png'
+        },
+        {
+          name: 'PSQL',
+          img: '/images/postgresql.svg'
+        },
+        {
+          name: 'Sequelize',
+          img: '/images/sequelize.svg'
+        },
+        {
+          name: 'JSON',
+          img: '/images/json.svg'
+        },
+        {
+          name: 'Ruby',
+          img: '/images/ruby.svg'
+        },
+        {
+          name: 'Rails',
+          img: '/images/rails.svg'
+        }
+      ]
+    }
+  }
+
+  approvedTech = () => {
+    const removeTech = ['Mysql', 'Github', 'jQuery', 'Sequelize', 'JSON'];
+    const editedTech = this.state.techList.filter(tech => (
+      !removeTech.includes(tech.name)
+    ))
+    return editedTech;
+  }
+
+  render() {
+    return (
+      <div id="technologies" className="technologies" >
+        {
+          this.approvedTech().map((tech, key) => (
+            <div key={key} className="tech-list">
+              <img src={tech.img} alt="tech-icon" className="tech-img"></img>
+              <div className="tech-name">{tech.name}</div>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default Technologies
