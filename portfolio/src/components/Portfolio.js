e37a8b4fd925392d19cbb962e4429177306f8fb8
nolanG <nolanrgiles@gmail.com>
2020-01-11 15:59:15 -0500

    Updated to have state array to contain portfolio projects and map through in render to display a grid

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,19 +1,65 @@
 import React from 'react'
 
-const Portfolio = () => {
-  return (
-    <div>
-      <div>
-        Project 1!
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      projects: [
+        {
+          img: '/images/test-img.png',
+          title: 'portfolio',
+          description: '',
+          tech: [],
+          link: '',
+          gitlink: '',
+        },
+        {
+          img: '/images/test-img.png',
+          title: 'blackjack!',
+          description: '',
+          tech: [],
+          link: 'https://blackjacknolan.surge.sh',
+          gitlink: '',
+        },
+        {
+          img: '/images/test-img.png',
+          title: 'Worth Watching',
+          description: '',
+          tech: [],
+          link: 'https://worthwatching.surge.sh',
+          gitlink: '',
+        },
+        {
+          img: '/images/test-img.png',
+          title: 'coming soon!',
+          description: '',
+          tech: [],
+          link: '',
+          gitlink: '',
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+
+      <div id="portfolio" className="portfolio">
+        {
+          this.state.projects.map((project, key) => (
+            <div className="portfolio-project" key={key}>
+              <div className="project-title">
+                {project.title}
+              </div>
+              <div className="project-link">
+                View the deployed project <a href={project.link} target="_blank" rel="noopener noreferrer">here</a>.
+              </div>
+            </div>
+          ))
+        }
       </div>
-      <div>
-        Project 2!
-      </div>
-      <div>
-        Project 3!
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Portfolio
