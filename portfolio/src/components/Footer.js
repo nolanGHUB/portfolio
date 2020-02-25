09db955ec04b208f423f3eef6370053273d2eb70
nolanGHUB <nolanrgiles@gmail.com>
2020-02-25 12:51:10 -0500

    [footer] Created footer to mimic technologies styling

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Nolan Giles - 2020 - LinkedIn - Github</p>
+      <div className="footer-title">
+        Contact
+      </div>
+      <p>Nolan Giles - 2020 - <span><a href='https://www.linkedin.com/in/nolangiles/'>LinkedIn</a></span> - <a href="https://github.com/nolanGHUB">Github</a></p>
     </div>
   )
 }
